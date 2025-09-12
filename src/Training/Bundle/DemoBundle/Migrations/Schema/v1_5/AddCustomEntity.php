f3ce0e733f609cb590dced16330358fd80cddccc
Bert Van Mieghem <bert.vanmieghem@cairox.be>
2025-09-12 12:08:05 +0200

    add custom entity

---
+++ src/Training/Bundle/DemoBundle/Migrations/Schema/v1_5/AddCustomEntity.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Training\Bundle\DemoBundle\Migrations\Schema\v1_5;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddCustomEntity implements Migration, ExtendExtensionAwareInterface
+{
+    protected ExtendExtension $extendExtension;
+
+    #[\Override]
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
+    }
+
+    #[\Override]
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $table = $this->extendExtension->createCustomEntityTable(
+            $schema,
+            'CustomEntity'
+        );
+        $table->addColumn(
+            'name',
+            'string',
+            [
+                'length' => 100,
+                'oro_options' => [
+                    'extend'  => ['owner' => ExtendScope::OWNER_CUSTOM],
+                    'entity' => ['label' => 'name']
+                ]
+            ]
+        );
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            $table,
+            'users',
+            'oro_user',
+            'username',
+            [
+                'extend' => [
+                    'owner' => ExtendScope::OWNER_CUSTOM
+                ]
+            ]
+        );
+    }
+
+}
