f4ed0f2d40bd59e20fc133a134cb5533476f3956
Bert Van Mieghem <bert.vanmieghem@cairox.be>
2025-09-12 11:06:10 +0200

    add enum field to user

---
+++ src/Training/Bundle/DemoBundle/Migrations/Data/ORM/LoadUserInternalRatingData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Training\Bundle\DemoBundle\Migrations\Data\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Oro\Bundle\EntityExtendBundle\Entity\EnumOption;
+use Oro\Bundle\EntityExtendBundle\Entity\Repository\EnumOptionRepository;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\TranslationBundle\Migrations\Data\ORM\LoadLanguageData;
+
+class LoadUserInternalRatingData extends AbstractFixture implements DependentFixtureInterface
+{
+    protected array $data = [
+        '1' => true,
+        '2' => false,
+        '3' => false,
+        '4' => false,
+        '5' => false
+    ];
+
+    #[\Override]
+    public function load(ObjectManager $manager): void
+    {
+        /** @var EnumOptionRepository $enumRepo */
+        $enumRepo = $manager->getRepository(EnumOption::class);
+        $priority = 1;
+        foreach ($this->data as $name => $isDefault) {
+            $enumOption = $enumRepo->createEnumOption(
+                'user_internal_rating',
+                ExtendHelper::buildEnumInternalId($name),
+                $name,
+                $priority++,
+                $isDefault,
+            );
+            $manager->persist($enumOption);
+        }
+
+        $manager->flush();
+    }
+
+    #[\Override]
+    public function getDependencies(): array
+    {
+        return [LoadLanguageData::class];
+    }
+}
---
+++ src/Training/Bundle/DemoBundle/Migrations/Schema/v1_3/AddEnumFieldOroUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Training\Bundle\DemoBundle\Migrations\Schema\v1_3;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class AddEnumFieldOroUser implements Migration, ExtendExtensionAwareInterface
+{
+    protected ExtendExtension $extendExtension;
+
+    #[\Override]
+    public function setExtendExtension(ExtendExtension $extendExtension): void
+    {
+        $this->extendExtension = $extendExtension;
+    }
+
+    #[\Override]
+    public function up(Schema $schema, QueryBag $queries): void
+    {
+        $table = $schema->getTable('oro_user');
+        $this->extendExtension->addEnumField(
+            $schema,
+            $table,
+            'internal_rating', // field name
+            'user_internal_rating', // enum code
+            false, // only one option can be selected
+            false, // an administrator can add new options and remove existing ones
+            [
+                'extend' => ['owner' => ExtendScope::OWNER_CUSTOM],
+                'entity' => ['label' => 'Internal rating']
+            ]
+        );
+    }
+}
